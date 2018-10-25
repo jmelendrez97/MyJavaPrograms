7e64c4064f38793a348b616691104e1d17399fef
jonathan.l.melendrez <jonathan.l.melendrez@accenture.com>
2018-10-25 09:39:23 +0800

    Signed-off-by: jonathan.l.melendrez <jonathan.l.melendrez@accenture.com>

--- FirstAndLastDigitSum/src/com/jonathan/s/melendrez/Main.java
+++ FirstAndLastDigitSum/src/com/jonathan/s/melendrez/Main.java
@@ -1,9 +1,26 @@
 package com.jonathan.s.melendrez;
 
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(sumFirstAndLastDigit(252));
     }
 
+    public static int sumFirstAndLastDigit(int number){
+        int digits[]={0,0};// 0 - first 1 -last
+        digits[0]=number;
+        if(number>=10){
+            digits[1]= number%10;
+            while (number>=10){
+                digits[0]=number/=10;
+            }
+            return digits[0]+digits[1];
+        }
+        else if (number>=0){
+            return 2*number;
+        }
+        return -1;
+    }
 }
